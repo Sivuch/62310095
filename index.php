041d27ab785dd65fb18ec66faff9435b287ac09e
Sivuch <59908434+Sivuch@users.noreply.github.com>
2020-01-24 21:30:37 +0700

    2nd commit

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
 <html>
-<body>
-
-<h1>My first PHP page</h1>
+    <head>
+        <title>My Frist PHP Website</title>
+    </head>
+    <body>
+        <h2>Registration Page</h2>
+        <a href="index.php">Click here to go back</a><br/><br/>
+        <form action="Register.php" method="post">
+            Enter Username: <input type="text" name="username" required="required"/> <br/>
+            Enter Password: <input type="password" name="password" required="required"/> <br/>
+            <intput type="submit" value="Register"/>
+        </form>
+    </body>
+</html>
 
 <?php
-echo "Hello World!";
-?>
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = mysql_real_escape_string($_POST['username']);
+    $password = mysql_real_escape_string($_POST['password']);
 
-</body>
-</html>
+    echo "Username entered is: ". $username . "<br/>";
+    echo "Password entered is: ". $password;
+}
+?>
